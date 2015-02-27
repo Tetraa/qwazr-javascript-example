e9065da36ce8b0e6f5795517df405f6374c697a8
Emmanuel Keller <ekeller@open-search-server.com>
2015-02-27 22:22:50 +0100

    Change csql to cql

--- controller/signin.js
+++ controller/signin.js
@@ -1,9 +1,23 @@
-login = request.getParameter("login");
-password = request.getParameter("password");
-if (login != null && password != null) {
-	message = "AUTH";
-} else {
-	message = "For this demo uses admin/admin";
+var message;
+var redirect;
+
+// Check the request body
+if (request.body != null) {
+	login = request.body.parameter("login");
+	password = request.body.parameter("password");
+	if (login != null && password != null) {
+		message = login + ' ' + password;
+		var row = providers.cassandra.execute("SELECT * FROM authors WHERE user_name=? AND password = ?", login, password).one();
+		redirect = request.getContextPath();
+	}
 }
-response.addVariable("message", message);
-response.view("signin.ftl");
+
+// The content of message will be displayed
+if (message  == null)
+	message = "For this demo use admin/admin";
+response.variable("message", message);
+
+if (redirect != null)
+	response.redirect(redirect);
+else //Display the template, a path relative to the /view directory
+	response.view("signin.ftl");
