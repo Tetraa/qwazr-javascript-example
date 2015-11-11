27c1c8866d028f1daf8a5ff97cd3cb02e7ab0aa2
Emmanuel Keller <ekeller@open-search-server.com>
2015-11-11 23:28:30 +0100

    Working Javascript example

--- controller/index.js
+++
@@ -1,4 +0,0 @@
-var authors = providers.cassandra.execute("select * from authors", 1);
-response.variable("authors", authors);
-response.variable("message", "Hello world!");
-response.view("index.ftl");
